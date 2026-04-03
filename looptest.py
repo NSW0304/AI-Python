9da38393e167059769a18afce9a1a9b456d62e58
NSW0304 <nsw0304@gmail.com>
2026-04-03 16:15:36 +0900

    Add files via upload

---
+++ looptest.py
@@ -0,0 +1,19 @@
+##oddnum=0
+##evennum=0
+##for i in range(1,101):
+##    if i%2!=0:
+##        oddnum=oddnum+i
+##    else:
+##        evennum=evennum+i
+##print("홀수합:", oddnum)
+##print("짝수합:", evennum)
+
+##sum=0
+##i=1
+##while sum<=3000:
+##    print(sum)
+##    sum=sum+i
+##    i+=1
+
+for i in "Hello":
+    print(i)
